b3f2883f5a531a8a1b79f441834757755c71be6b
Guillermo Miravalles <miravallesg@gmail.com>
2021-01-21 18:17:52 -0300

    2. React Elements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'; //libreria de react
+import ReactDOM from 'react-dom'; //glue between react & DOM
+import './index.css'; //styles
+import App from './App'; //componente App that we are rendering
 import reportWebVitals from './reportWebVitals';
 
+const greeting = React.createElement('h1',{}, 'Hello World!')
+
 ReactDOM.render(
+  greeting,
+  document.getElementById('root')
+);
+
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+); */
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
